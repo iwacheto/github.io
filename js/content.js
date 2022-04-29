47edcfb4bde0c7284a0e17b73abe055940f0e4a1
nCuXaP <nupoha@gmail.com>
2022-04-29 20:34:34 +0300

    Add new pag

--- js/content.js
+++ js/content.js
@@ -14,7 +14,13 @@ $(function () {
     let ser_par2 = $(".ser_par2").text("You need of Application? I make Web Application and, which can make your life easier.");
     let ser_par3 = $(".ser_par3").text("You need for beautiful Website i can make for you!");
     let ser_par4 = $(".ser_par4").text("SEO is one of most important thing for Websites.Google, Facebook, Instagram and Twitter will find your Website on 100%!");
+    let ser_par5 = $(".ser_par5").text("I really care about your time and my job is tailored with your requirements.");
+    let ser_par6 = $(".ser_par6").text("Everithing in your Website will be exactly that you want on the last pixel.");
     let h2_one = $(".h2_one").text("Who am I?");
+    let heading_port = $(".heading_port").text("Portfolio");
+    let footer_h = $(".footer_h").text("For contacts with me");
+    let email_par = $(".email_par").text("Email: iwacheto@abv.bg");
+    let phone_par = $(".phone_par").text(" Phone:  +359 882948590");
     let par_1 = $(".par_1").text("I've worked as Web developer for two years and I love my work. I live in Pleven,Bulgaria.");
     let par_2 = $(".par_2").text("I create beautiful landing pages and Web application using the latest tecnologies.");
     console.log(h3_one);
@@ -39,7 +45,13 @@ $(function () {
             ser_par2 = $(".ser_par2").text("You need of Application? I make Web Application and, which can make your life easier.");
             ser_par3 = $(".ser_par3").text("You need for beautiful Website i can make for you!");
             ser_par4 = $(".ser_par4").text("SEO is one of most important thing for Websites.Google, Facebook, Instagram and Twitter will find your Website on 100%!");
+            ser_par5 = $(".ser_par5").text("I really care about your time and my job is tailored with your requirements.");
+            ser_par6 = $(".ser_par6").text("Everithing in your Website will be exactly that you want on the last pixel.");
             h2_one = $(".h2_one").text("Who am I?");
+            footer_h = $(".footer_h").text("For contacts with me");
+            heading_port = $(".heading_port").text("Portfolio");
+            email_par = $(".email_par").text("Email: iwacheto@abv.bg");
+            phone_par = $(".phone_par").text(" Phone:  +359 882948590");
              par_1 = $(".par_1").text("I've worked as Web developer for two years and I love my work. I live in Pleven,Bulgaria.");
             par_2 = $(".par_2").text("I create beautiful landing pages and Web application using the latest tecnologies.");
         } else {
@@ -58,7 +70,13 @@ $(function () {
             ser_par2 = $(".ser_par2").text("Нуждаете се от Уеб приложение за вашия бизнес? Можете да ме потърсите и заедно ще намерим решение.");
             ser_par3 = $(".ser_par3").text("Искате да имате красив Уебсайт? Аз ще се погрижа да удовлетворя изискванията ви!");
             ser_par4 = $(".ser_par4").text("Оптмизацията е много важно в наши дни! Вашият Уебсайт ще бъде добре индексиран от търсещите машини на Google, Facebook, Twitter и Instagram.");
+            ser_par5 = $(".ser_par5").text("Наистина ценя времето на клиентите си и се стремя да спазвам възможно най-кратки срокове.");
+            ser_par6 = $(".ser_par6").text("Всичко във вашия дизайн трябва да е точно до последния пиксел!");
             h2_one = $(".h2_one").text("Кой съм аз?");
+            heading_port = $(".heading_port").text("Портфолио");
+            footer_h = $(".footer_h").text("За контакти с мен");
+            email_par = $(".email_par").text("Имейл: iwacheto@abv.bg");
+            phone_par = $(".phone_par").text("Телефон:  +359 882948590");
             par_1 = $(".par_1").text("Работя като Уеб дизайнер от две години и обичам работата си. Живея в Плевен.");
             par_2 = $(".par_2").text("Създавам красиви Уебсайтове и Уеб приложения,използвайки последните технологии.");
         }
